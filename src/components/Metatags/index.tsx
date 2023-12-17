0819585cfe813799a8225a0628a26b7f1ddf9de1
Ny Nivoarijaona <nynivoarijaona@gmail.com>
2023-12-17 22:12:18 +0300

    try to change open graph protocol

--- src/components/Metatags/index.tsx
+++ src/components/Metatags/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import imageMeta from "../../images/face.webp";
 import iconTag from "../../images/favicon.png";
 
 function Metatags() {
@@ -21,7 +20,7 @@ function Metatags() {
         property="og:description"
         content="A young boy passionate of IT and UI/UX. I'm a Developer Front End and a Web Designer too."
       />
-      <meta property="og:image" content={imageMeta} />
+      <meta property="og:image" content="https://ny-nivoarijaona.com/static/face-50152a0e05b4cb3c4274331c84783af2.webp" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://www.ny-nivoarijaona.com" />
@@ -33,7 +32,7 @@ function Metatags() {
         property="twitter:description"
         content="A young boy passionate of IT and UI/UX. I'm a Developer Front End and a Web Designer too."
       />
-      <meta property="twitter:image" content={imageMeta} />
+      <meta property="twitter:image" content="https://ny-nivoarijaona.com/static/face-50152a0e05b4cb3c4274331c84783af2.webp" />
 
       <meta
         name="description"
